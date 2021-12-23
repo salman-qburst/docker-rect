1e07cfe7a437ec03942a86de0aa48cd4218526d1
salman faris k <salman.faris@qburst.com>
2021-12-23 18:46:12 +0530

    initail commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi there.! From docker hi salman dfffffffffffff
+
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
