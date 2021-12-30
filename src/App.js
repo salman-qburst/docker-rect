55a82f301f1fd13c2e5b1e939aaebcffacec1c95
salman faris k <salman.faris@qburst.com>
2021-12-30 10:28:58 +0530

    change from feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hi there from feature branch
-          am new change
+          change from feature
 
         </p>
        
