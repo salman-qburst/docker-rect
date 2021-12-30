728b318e90c28153268f1cd4a0669b231b044713
salman faris k <salman.faris@qburst.com>
2021-12-30 10:21:26 +0530

    app js change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there
+          Hi there from feature branch
+          am new change
 
         </p>
        
