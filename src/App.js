0c7da81a690f7319545f1fe415da225d668bfaa6
salman faris k <salman.faris@qburst.com>
2021-12-30 09:40:41 +0530

    build file and test file editing

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there from feature branch
-          am new change
+          Hi there
 
         </p>
        
